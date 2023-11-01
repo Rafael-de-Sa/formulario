5eb0a37541dac512ab3438b709a296aa853199b0
Rafael de Sá <rafaeldesa170622@gmail.com>
2023-11-01 00:52:21 -0300

    parcial 1

--- cabecalho.php
+++ cabecalho.php
@@ -6,9 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Web 2</title>
-    
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <!--Utilizando css e js do boostrap versão 5.3.2 - via arquivo-->
+    <link rel="stylesheet" href="./bootstrap-5.3.2-dist/css/bootstrap.min.css">
+    <script src="./bootstrap-5.3.2-dist/js/bootstrap.bundle.min.js"></script>
+
     <link rel="stylesheet" href="style.css" type="text/css">
     <script src="https://kit.fontawesome.com/d6ea71334b.js" crossorigin="anonymous"></script>
 </head>
